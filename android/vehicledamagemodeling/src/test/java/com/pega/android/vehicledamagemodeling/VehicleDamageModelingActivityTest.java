d6291fd89a6b155a22db2c52457245e21ebacfcc
domaj <Jakub.Domanski@pega.com>
2019-01-03 09:24:12 +0100

    BUG-418686 moved unit test to library; Jakub Domanski

---
+++ android/vehicledamagemodeling/src/test/java/com/pega/android/vehicledamagemodeling/VehicleDamageModelingActivityTest.java
@@ -0,0 +1,13 @@
+package com.pega.android.vehicledamagemodeling;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VehicleDamageModelingActivityTest {
+
+    @Test
+    public void onCreate() {
+        assertTrue(true);
+    }
+}
