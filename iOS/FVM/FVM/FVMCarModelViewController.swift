5bda85f991c3e310266f71146fe796dc9ab46b45
Grzesiek Hawliczek <ghawliczek@outlook.com>
2019-01-08 16:36:47 +0100

    [i] Added lights. Changed onTapGesture to color parts instead of scaling. Amended SCNView to fit the whole screen of a device

--- iOS/FVM/FVM/FVMCarModelViewController.swift
+++ iOS/FVM/FVM/FVMCarModelViewController.swift
@@ -18,7 +18,7 @@ import SceneKit
 open class FVMCarModelViewController : SCNView {
     var scnScene: SCNScene!
     var scnCamera : SCNNode!
-    var highlightedParts = [SCNNode]()
+    var highlightedParts = [(node: SCNNode, material: Any?)]()
     
     public func onStartup() {
         self.allowsCameraControl = true
@@ -31,7 +31,29 @@ open class FVMCarModelViewController : SCNView {
 
     func setupScene() {
         scnScene = SCNScene(named: "art.scnassets/model.scn")
+        scnScene.background.contents = "art.scnassets/background.png"
         self.scene = scnScene
+        
+        scnCamera = SCNNode()
+        scnCamera.camera = SCNCamera()
+        scnCamera.position = SCNVector3(x: 0, y: 0, z: 10)
+        scnCamera.camera?.zFar = 50
+        scnScene.rootNode.addChildNode(scnCamera)
+        
+        let topLight = SCNNode()
+        topLight.light = SCNLight()
+        topLight.light?.type = .directional
+        topLight.position = SCNVector3(x: 0, y: 50, z: 0)
+        topLight.eulerAngles = SCNVector3Make(Float(-Double.pi / 2), 0, 0)
+        scnScene.rootNode.addChildNode(topLight)
+        
+        let bottomLight = SCNNode()
+        bottomLight.light = SCNLight()
+        bottomLight.light?.type = .directional
+        bottomLight.light?.intensity = 750
+        bottomLight.position = SCNVector3(x: 0, y: -50, z: 0)
+        bottomLight.eulerAngles = SCNVector3Make(Float(-Double.pi / 2), 0, 0)
+        scnScene.rootNode.addChildNode(bottomLight)
     }
     
     @objc
@@ -40,17 +62,19 @@ open class FVMCarModelViewController : SCNView {
         let hitResults = self.hitTest(p, options: [:])
         if hitResults.count > 0 {
             let result = hitResults.first!
-            if highlightedParts.contains(result.node) {
+            let highlightedNodes = highlightedParts.map { $0.0 }
+            if highlightedNodes.contains(result.node) {
                 SCNTransaction.begin()
                 SCNTransaction.animationDuration = 0.5
-                highlightedParts.remove(at: highlightedParts.firstIndex(of: result.node)!)
-                result.node.scale = SCNVector3(x: 1, y: 1, z: 1)
+                let hitPartIndex = highlightedNodes.firstIndex(of: result.node)
+                result.node.geometry?.firstMaterial?.diffuse.contents = highlightedParts[hitPartIndex!].material
+                highlightedParts.remove(at: highlightedNodes.firstIndex(of: result.node)!)
                 SCNTransaction.commit()
             } else {
                 SCNTransaction.begin()
                 SCNTransaction.animationDuration = 0.5
-                highlightedParts.append(result.node)
-                result.node.scale = SCNVector3(x: 2, y: 2, z: 2)
+                highlightedParts.append((node: result.node, material: result.node.geometry?.firstMaterial?.diffuse.contents))
+                result.node.geometry?.firstMaterial?.diffuse.contents = UIColor.red
                 SCNTransaction.commit()
             }
         } else {
@@ -59,10 +83,10 @@ open class FVMCarModelViewController : SCNView {
     }
     
     func setHighlightsOff() {
-        for part in highlightedParts {
+        for tuple in highlightedParts {
             SCNTransaction.begin()
             SCNTransaction.animationDuration = 0.5
-            part.scale = SCNVector3(x: 1, y: 1, z: 1)
+            tuple.node.geometry?.firstMaterial?.diffuse.contents = tuple.material
             SCNTransaction.commit()
         }
         highlightedParts.removeAll()
