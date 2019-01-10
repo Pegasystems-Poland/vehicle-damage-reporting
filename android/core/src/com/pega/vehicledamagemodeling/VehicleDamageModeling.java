2b35b5033b43e6c657d4d626fe70b8afb8ccf0d9
domaj <Jakub.Domanski@pega.com>
2019-01-10 20:08:41 +0100

    [a] US-256039 displaying 3D model; Jakub Domanski

--- android/core/src/com/pega/vehicledamagemodeling/VehicleDamageModeling.java
+++ android/core/src/com/pega/vehicledamagemodeling/VehicleDamageModeling.java
@@ -16,17 +16,30 @@ package com.pega.vehicledamagemodeling;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.utils.Array;
 import com.google.gson.JsonObject;
 
-public class VehicleDamageModeling extends ApplicationAdapter {
 
+public class VehicleDamageModeling extends ApplicationAdapter {
     private final VehicleDamageReportCallback callback;
 
-    private SpriteBatch batch;
-    private Texture img;
+    private PerspectiveCamera cam;
+    private CameraInputController camController;
+    private ModelBatch modelBatch;
+    private AssetManager assets;
+    private Array<ModelInstance> instances = new Array<>();
+    private Environment environment;
+    private boolean loading;
 
     public VehicleDamageModeling(VehicleDamageReportCallback callback) {
         this.callback = callback;
@@ -41,23 +54,64 @@ public class VehicleDamageModeling extends ApplicationAdapter {
     }
 
     @Override
-    public void create() {
-        batch = new SpriteBatch();
-        img = new Texture("cat.jpg");
+    public void create () {
+        modelBatch = new ModelBatch();
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(20f, 20f, 20f);
+        cam.lookAt(0,0,0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+
+        assets = new AssetManager();
+        assets.load("modelFF.obj", Model.class);
+        loading = true;
+    }
+
+    private void doneLoading() {
+        Model car = assets.get("modelFF.obj", Model.class);
+        ModelInstance shipInstance = new ModelInstance(car);
+        instances.add(shipInstance);
+        loading = false;
     }
 
     @Override
-    public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+    public void render () {
+        if (loading && assets.update())
+            doneLoading();
+        camController.update();
+
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(cam);
+        modelBatch.render(instances, environment);
+        modelBatch.end();
     }
 
     @Override
-    public void dispose() {
-        batch.dispose();
-        img.dispose();
+    public void dispose () {
+        modelBatch.dispose();
+        instances.clear();
+        assets.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
     }
 }
