4dcaeee24707f1b71b92f55b8e4d348f9ad3f180
Szymon Smykała <ctk7000@gmail.com>
2019-04-13 19:48:37 +0200

    removed useless test

--- iOS/FVM/Framework/Controllers/FVMDamagedCarViewController.swift
+++ iOS/FVM/Framework/Controllers/FVMDamagedCarViewController.swift
@@ -93,7 +93,6 @@ public class FVMDamagedCarViewController: UIViewController {
     fileprivate func hideRotationPrompt() {
         for view in self.view.subviews {
             if view.restorationIdentifier?.isEqual(ROTATION_PROMPT) ?? false as Bool {
-//                view.removeFromSuperview()
                 view.alpha = 0
             }
         }
