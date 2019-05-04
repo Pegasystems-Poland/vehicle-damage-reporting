c6001eb642cc1e29b2c2a7975c15701572fcdef4
Kamil Czajka <37740125+lapwingg@users.noreply.github.com>
2019-05-04 13:34:15 +0200

    BUG-438322 Added missing separator between the description and model area (#65)

--- iOS/FVM/Framework/Controllers/FVMDamagedCarViewController.swift
+++ iOS/FVM/Framework/Controllers/FVMDamagedCarViewController.swift
@@ -21,13 +21,13 @@ public class FVMDamagedCarViewController: UIViewController {
     @IBOutlet weak var userPromptText: UITextView!
     @IBOutlet weak var closeButton: UIButton!
     @IBOutlet weak var acceptButton: UIButton!
+    @IBOutlet weak var userInteractionElemsView: UIView!
     public var configuration: String!
     public var completionAction: ((String) -> Void)?
-    
+
     override public func viewDidLoad() {
         setupDamagedCarScene()
-        showRotationPrompt()
-        
+
         super.viewDidLoad()
     }
     
@@ -53,13 +53,20 @@ public class FVMDamagedCarViewController: UIViewController {
     private func setupDamagedCarScene() {
         damageSelector.onStartup(configuration: configuration)
         
+        showRotationPrompt()
         fillUserPromptTextView()
         disableAcceptButton()
         addAcceptButtonObservers()
+        addBordersForView(userInteractionElemsView)
     }
     
     private func showRotationPrompt() {
         NotificationCenter.default.addObserver(self, selector: #selector(hideRotationPrompt), name: .hideRotationPrompt, object: nil)
+    }
+    
+    private func addBordersForView(_ view: UIView) {
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.lightGray.cgColor
     }
     
     private func addAcceptButtonObservers() {
